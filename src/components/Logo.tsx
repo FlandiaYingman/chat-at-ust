ff2d13c411c4f3e84805060eca7a0e7b46fdfa20
Flandia Yingman <me@flandia.dev>
2024-01-15 05:12:49 +0800

    fix: React does not recognize the `ActionAreaProps` prop on a DOM element.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,9 +5,12 @@ import { useNavigate } from "react-router";
 
 export default function Logo(props: CardProps & { ActionAreaProps?: CardActionAreaProps }): ReactElement {
   const { ActionAreaProps = {} } = props;
+  const CardProps = { ...props };
+  delete CardProps.ActionAreaProps;
+
   const navigate = useNavigate();
   return (
-    <Card {...props} elevation={0}>
+    <Card {...CardProps} elevation={0}>
       <CardActionArea
         {...ActionAreaProps}
         onClick={() => {

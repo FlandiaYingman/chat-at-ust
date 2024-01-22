5eaee562260b2ffef840d31ae899464e4b9a5c01
Flandia Yingman <me@flandia.dev>
2024-01-22 19:20:59 +0800

    feat: apply `rehypeHighlight` for syntax highlighting

--- src/components/ChatMarkdown.tsx
+++ src/components/ChatMarkdown.tsx
@@ -1,44 +1,14 @@
-import "./ChatMarkdown.css";
 import { Typography, TypographyProps } from "@mui/material";
-import "katex/dist/katex.min.css";
 import { DetailedHTMLProps, HTMLAttributes, memo, type ReactElement } from "react";
 import Markdown, { type Options as MarkdownOptions } from "react-markdown";
-import SyntaxHighlighter, { type SyntaxHighlighterProps } from "react-syntax-highlighter";
-import { github } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import rehypeHighlight from "rehype-highlight";
 import rehypeKatex from "rehype-katex";
 import remarkGfm from "remark-gfm";
 import remarkMath from "remark-math";
+import "highlight.js/styles/github.css";
+import "katex/dist/katex.min.css";
+import "./ChatMarkdown.css";
 
-function code(): (props: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>) => ReactElement {
-  // eslint-disable-next-line react/display-name
-  return (props) => {
-    // eslint-disable-next-line react/prop-types
-    const { children, className, ...rest } = props;
-    if (/language-math/.exec(className ?? "") != null)
-      return (
-        <code {...rest} className={className}>
-          {children}
-        </code>
-      );
-    const match = /language-(\w+)/.exec(className ?? "");
-    return match != null ? (
-      <SyntaxHighlighter
-        {...(rest as SyntaxHighlighterProps)}
-        language={match[1]}
-        style={github}
-        customStyle={{ padding: 0, overflowX: "unset" }}
-        PreTag="div"
-        CodeTag="code"
-      >
-        {String(children).replace(/\n$/, "")}
-      </SyntaxHighlighter>
-    ) : (
-      <code {...rest} className={className}>
-        {children}
-      </code>
-    );
-  };
-}
 
 function header(
   h: "h1" | "h2" | "h3" | "h4" | "h5" | "h6",
@@ -87,9 +57,11 @@ export const ChatMarkdown = memo(function ChatMarkdown(
       className="chat-markdown"
       {...props}
       remarkPlugins={[remarkMath, remarkGfm]}
-      rehypePlugins={[[rehypeKatex, {}]]}
+      rehypePlugins={[
+        [rehypeKatex, {}],
+        [rehypeHighlight, { detect: true }]
+      ]}
       components={{
-        code: code(),
         h1: header("h1", props),
         h2: header("h2", props),
         h3: header("h3", props),

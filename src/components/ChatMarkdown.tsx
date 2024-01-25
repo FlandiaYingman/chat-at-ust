56ae6e5e40e1bb3c014cde920f656c9773dbd430
Flandia Yingman <me@flandia.dev>
2024-01-25 21:59:41 +0800

    feat: add copy button to code blocks

--- src/components/ChatMarkdown.tsx
+++ src/components/ChatMarkdown.tsx
@@ -1,5 +1,5 @@
-import { Typography, TypographyProps } from "@mui/material";
-import { DetailedHTMLProps, HTMLAttributes, memo, type ReactElement } from "react";
+import { Card, CardActionArea, Fade, Snackbar, Typography, TypographyProps } from "@mui/material";
+import { DetailedHTMLProps, HTMLAttributes, memo, useState, type ReactElement } from "react";
 import Markdown, { type Options as MarkdownOptions } from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import rehypeKatex from "rehype-katex";
@@ -8,6 +8,7 @@ import remarkMath from "remark-math";
 import "highlight.js/styles/github.css";
 import "katex/dist/katex.min.css";
 import "./ChatMarkdown.css";
+import { ContentCopy } from "@mui/icons-material";
 
 
 function header(
@@ -43,6 +44,59 @@ function paragraph(
   };
 }
 
+function pre(): (props: DetailedHTMLProps<HTMLAttributes<HTMLElement>, HTMLElement>) => ReactElement {
+  const textOf = (node: unknown): string => {
+    if (!node) return "";
+    if (typeof node === "string") return node;
+    if (typeof node === "number") return node.toString();
+    if (typeof node === "boolean") return "";
+    if (typeof node === "object") {
+      if (node instanceof Array) return node.map(textOf).join("");
+      if ("props" in node) {
+        // @ts-expect-error: it should have children props.
+        // If it doesn't have, it will pass a falsy value which returns empty string.
+        return textOf(node.props.children);
+      }
+    }
+    console.warn("Unresolved `node` of type:", typeof node, node);
+    return "";
+  };
+  // eslint-disable-next-line react/display-name
+  return (props) => {
+    // eslint-disable-next-line react/prop-types
+    const { children, className } = props;
+    const [showCopyButton, setShowCopyButton] = useState(false);
+    const [showCopiedSnackbar, setShowCopiedSnackbar] = useState(false);
+    return (
+      <pre
+        className={className}
+        style={{ position: "relative" }}
+        onMouseEnter={() => setShowCopyButton(true)}
+        onMouseLeave={() => setShowCopyButton(false)}
+      >
+        {children}
+        <Fade in={showCopyButton}>
+          <Card sx={{ position: "absolute", top: 8, right: 8 }}>
+            <CardActionArea
+              sx={{ height: 28, width: 28, display: "flex", justifyContent: "center", alignItems: "center" }}
+              onClick={() => navigator.clipboard.writeText(textOf(children)).then(() => setShowCopiedSnackbar(true))}
+            >
+              <ContentCopy sx={{ color: "text.secondary", width: 20, height: 20 }} />
+            </CardActionArea>
+          </Card>
+        </Fade>
+        <Snackbar
+          open={showCopiedSnackbar}
+          onClose={() => setShowCopiedSnackbar(false)}
+          autoHideDuration={3000}
+          message="Copied the content of code block to clipboard! "
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        />
+      </pre>
+    );
+  };
+}
+
 type ChatMarkdownProps = {
   noGutter?: boolean;
   base?: "body1" | "body2";
@@ -72,6 +126,7 @@ export const ChatMarkdown = memo(function ChatMarkdown(
         ul: paragraph(base, "ul", props),
         ol: paragraph(base, "ol", props),
         table: paragraph(base, "table", props),
+        pre: pre()
       }}
     />
   );
